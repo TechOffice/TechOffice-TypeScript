2137b0ff53cf4a7d0e73ddfba8999e647eaaf902
ben <ben@techoffice.com>
2018-06-14 01:16:38 +0800

    update

---
+++ 020-Angular/999-Dev/070-Editor/src/AppModule.ts
@@ -0,0 +1,28 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import AppComponent from './AppComponent';
+import AppDetailComponent from './AppDetailComponent';
+
+
+@NgModule({
+    declarations: [
+      AppComponent,
+      AppDetailComponent
+    ],
+    entryComponents:[
+      
+    ],
+    imports: [
+      BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+  })
+export default class AppModule {
+    
+
+
+}
+
+
