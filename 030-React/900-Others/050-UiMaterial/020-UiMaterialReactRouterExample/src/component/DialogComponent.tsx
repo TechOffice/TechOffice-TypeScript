f1c23f3e55349ed00d49dd43ee5a433abee6c1ef
ben <ben@techoffice.com>
2018-10-25 01:00:27 +0800

    update

--- 030-React/900-Others/050-UiMaterial/020-UiMaterialReactRouterExample/src/component/DialogComponent.tsx
+++ 030-React/900-Others/050-UiMaterial/020-UiMaterialReactRouterExample/src/component/DialogComponent.tsx
@@ -1,10 +1,25 @@
 import * as React from "react";
+import Button from "@material-ui/core/Button/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 export default class DialogComponent extends React.Component<any, any>{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            open: false
+        };
+    }
 
     render(){
         return (
             <div>
+                <h1>Dialog</h1>
+                <Button variant="contained" onClick={()=> this.setState({open: true})}>Open Dialog</Button>
+                <Dialog open={this.state.open} onClose={()=>this.setState({open: false})}>
+                    <DialogTitle>Dialog Example</DialogTitle>
+                </Dialog>
             </div>
         )
     }
