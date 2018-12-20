cc2d570a97d9b6865930d69ec33834c202f012c1
ben <ben@techoffice.com>
2018-12-21 00:51:28 +0800

    update

--- 030-React/900-Others/060-ReduxForm/040-ReduxFormMaterialUi2/src/SimpleFormComponent.tsx
+++ 030-React/900-Others/060-ReduxForm/040-ReduxFormMaterialUi2/src/SimpleFormComponent.tsx
@@ -3,18 +3,32 @@ import { Field, reduxForm, FormProps, FormErrors, InjectedFormProps } from 'redu
 import TextField from './TextField';
 import { connect } from 'react-redux';
 
-
+@(connect( 
+    // map state to props
+    function(state: any){
+        console.log(state);
+        return {simple: state && state.form && state.form.simple && state.form.simple.values ? state.form.simple.values : {} };
+    } ,
+    // map dispatch to props
+    function(dispatch){
+        return {};
+    }    
+) as any)
 @(reduxForm({form : 'simple'}) as any)
-export default class SimpleFormComponent extends React.Component<InjectedFormProps | {} , any >{
+export default class SimpleFormComponent extends React.Component<InjectedFormProps | any , any >{
 
     constructor(props){
         super(props);
+        this.state = props;
     }
 
     render(){
         return (
             <form >
                 <Field name="firstName" component={TextField}/>
+                <div>
+                    {this.props.simple.firstName}
+                </div>
             </form>
         );
     }
